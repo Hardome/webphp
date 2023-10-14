e900fa37e99e883802ffa46cc0b2836591352096
Semyon Bedrin <s.o.bedrin@esoft.local>
2023-10-14 12:24:12 +0500

    homework/3

--- actions/upPageActions.php
+++ actions/upPageActions.php
@@ -4,16 +4,14 @@ if (isset($_POST['page'])) {
 	$fileContent = file_get_contents('../counter.txt');
 	$page = $_POST['page'];
 
-			$counters = json_decode($fileContent, true); // Преобразование данных в ассоциативный массив
+			$counters = json_decode($fileContent, true);
 			
-			// Изменение значения счетчика
 			if (isset($counters['actions'][$page])) {
 					$counters['actions'][$page]++;
 			} else {
 					$counters['actions'][$page] = 1;
 			}
 			
-			// Сохранение данных счетчика в файл
 			$fileData = json_encode($counters);
 			file_put_contents('../counter.txt', $fileData);
 
