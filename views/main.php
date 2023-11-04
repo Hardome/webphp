576d50fd81ab449aa74e42ca2843c95c0c652b66
Semyon Bedrin <s.o.bedrin@esoft.local>
2023-11-04 12:48:30 +0500

    homework/6

--- views/main.php
+++ views/main.php
@@ -1,49 +1,47 @@
 <?php
-  include 'config.php';
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  $typesSQL = "SELECT DISTINCT type from cars";
+include 'config.php';
+$conn = new mysqli($servername, $username, $password, $dbname);
+$typesSQL = "SELECT DISTINCT type from cars";
 
-  $types = mysqli_query($conn, $typesSQL);
-
+$types = mysqli_query($conn, $typesSQL);
 ?>
 
 <link rel='stylesheet' href='../views/styles.css'>
 
 <script>
+  function getMarks(value) {
+    const mark = value;
 
-function getMarks(value) {
-  const mark = value;
-
-  if (mark) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("mark").innerHTML = this.responseText;
-      }
-    };
-    xmlhttp.open("GET", "getMarks.php?type=" + mark, true);
-    xmlhttp.send();
+    if (mark) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("mark").disabled = false;
+          document.getElementById("mark").innerHTML = this.responseText;
+          calculate();
+        }
+      };
+      xmlhttp.open("GET", "getMarks.php?type=" + mark, true);
+      xmlhttp.send();
+    }
   }
 
-}
+  function calculate() {
+    const type = document.getElementById('type').value;
+    const mark = document.getElementById('mark').value;
+    const distance = document.getElementById('distance').value;
 
-
-function calculate(str) {
-  const type = document.getElementById('type').value;
-  const mark = document.getElementById('mark').value;
-  const distance = document.getElementById('distance').value;
-
-  if (type && mark && distance) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("result").value = this.responseText;
-      }
-    };
-    xmlhttp.open("GET", "calculate.php?type=" + type + "&mark=" + mark + "&distance=" + distance, true);
-    xmlhttp.send();
+    if (type && mark && distance) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("result").value = this.responseText;
+        }
+      };
+      xmlhttp.open("GET", "calculate.php?type=" + type + "&mark=" + mark + "&distance=" + distance, true);
+      xmlhttp.send();
+    }
   }
-}
 </script>
 
 <form action="">
@@ -51,12 +49,12 @@ function calculate(str) {
   <select id="type" onchange="getMarks(this.value)">
     <option value="" selected disabled hidden>выберите</option>
     <?php foreach ($types as $type) { ?>
-			<option value="<?= $type['type'] ?>"><?= $type['type'] ?></option>
-		<?php } ?>
+      <option value="<?= $type['type'] ?>"><?= $type['type'] ?></option>
+    <?php } ?>
   </select>
 
   <label for="mark">Марка:</label>
-  <select id="mark" onchange="calculate()">
+  <select id="mark" disabled onchange="calculate()">
     <option value="" selected disabled hidden>выберите</option>
   </select>
 
@@ -64,5 +62,5 @@ function calculate(str) {
   <input type="number" id="distance" min="0" onchange="calculate()">
 
   <label for="result">Расход бензина:</label>
-  <input id="result">
+  <input disabled id="result">
 </form>
